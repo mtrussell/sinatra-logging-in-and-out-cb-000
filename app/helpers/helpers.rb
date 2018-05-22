be906cdf2d7856ee697475ce4cd6348352829a4b
Matt Trussell <smtrussell@gmail.com>
2018-05-22 03:58:17 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,8 +1,9 @@
 class Helpers
   def current_user(sesh_hash)
-    @user = User.find_by(id: session[:id])
+    @user = User.find_by(id: sesh_hash[:id])
   end
 
-  def is_logged_in?
+  def is_logged_in?(sesh_hash)
+    
   end
 end
